db11dfe4ce4a3d43365a2314dbb885cf50885a38
lmw <alsdndjdrktl@gmail.com>
2021-09-05 20:15:56 +0900

    typeorm and nest #3

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,19 +1,36 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
-import { CreateRestaurantDto } from './dtos/create-restauran.dto'
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto'
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto'
 import { Restaurant } from './entities/restaurant.entity'
 import { RestaurantService } from './restaurants.service'
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query((returns) => [Restaurant])
   restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll()
   }
 
   @Mutation((returns) => Boolean)
-  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
     console.log(createRestaurantDto)
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto)
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
     return true
   }
 }
