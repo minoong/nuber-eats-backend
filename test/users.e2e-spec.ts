675f518839d9571ceeba95111c5d338442e8e5db
lmw <alsdndjdrktl@gmail.com>
2021-09-15 21:55:25 +0900

    user module E2E #login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,8 +4,21 @@ import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
 import { getConnection } from 'typeorm'
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  }
+})
+
+const GRAPHQL_ENDPOINT = '/graphql'
+const testUser = {
+  email: 'test@naver.com',
+  password: '123123',
+}
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication
+  let token: string
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +34,125 @@ describe('UserModule (e2e)', () => {
     app.close()
   })
 
-  it.todo('createAccount')
+  describe('createAccount', () => {
+    const EMAIL = '0915@account.com'
+    const PASSWORD = '11'
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email : "${testUser.email}",
+              password:"${testUser.password}"
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true)
+          expect(res.body.data.createAccount.error).toBe(null)
+        })
+    })
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email : "${testUser.email}",
+              password:"${testUser.password}"
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false)
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already.',
+          )
+        })
+    })
+  })
+
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          login(input:{
+            email : "${testUser.email}",
+            password:"${testUser.password}"
+          }) {
+            ok
+            token
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res
+
+          expect(login.ok).toBe(true)
+          expect(login.error).toBe(null)
+          expect(login.token).toEqual(expect.any(String))
+          token = login.token
+        })
+    })
+    it('should not be able to login with wrong credentails', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          login(input:{
+            email : "${testUser.email}",
+            password:"wrong"
+          }) {
+            ok
+            token
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res
+
+          expect(login.ok).toBe(false)
+          expect(login.error).toBe('wrong password.')
+          expect(login.token).toBe(null)
+        })
+    })
+  })
+
   it.todo('userProfile')
-  it.todo('login')
   it.todo('me')
   it.todo('verifyEmail')
   it.todo('editProfile')
